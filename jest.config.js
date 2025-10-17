43ea390cacdb637cb2962cc919c10861ca57a3c0
GreenXred <antonzo09@mail.ru>
2025-10-17 16:35:01 +0300

    Прописаны два интеграционных теста (пока проваливаются)

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 export default {
     testEnvironment: 'jsdom',
     transform: { '^.+\\.(js|jsx)$': 'babel-jest' },
+    extensionsToTreatAsEsm: ['.jsx'], // чтобы jest относился к JSX как к ESM
     moduleNameMapper: {
-        '\\.(css|scss)$': 'identity-obj-proxy',
-        '\\.(svg|png|jpg|jpeg|gif)$': '<rootDir>/fileMock.js'
+        '\\.(css|scss)$': 'identity-obj-proxy',// Игнорируем стили
+        '\\.(svg|png|jpg|jpeg|gif|webp|avif)$': '<rootDir>/test/mocks/fileMock.js', // Mock для изображений
+        '^.+products\\.json$': '<rootDir>/test/mocks/products.json' // Mock для products.json
     },
     setupFilesAfterEnv: ['@testing-library/jest-dom']
 };
